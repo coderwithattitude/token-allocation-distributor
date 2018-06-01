d97b8136e0251d64da3db0ce9d092929b6f6ce51
Anthony <adibas03@ymail.com>
2018-06-01 09:18:32 +0100

    optimize stakeHolderswight by using mapping

--- contracts/WeightedTokenDistributor.sol
+++ contracts/WeightedTokenDistributor.sol
@@ -5,7 +5,7 @@ import "./TokenDistributor.sol";
 contract WeightedTokenDistributor is TokenDistributor {
     using SafeMath for uint;
 
-    uint256 [] stakeHoldersWeight;
+    mapping( address => uint256) stakeHoldersWeight;
 
     constructor ( address _targetToken, uint256 _totalStakeHolders, address[] _stakeHolders, uint256[] _weights) public Ownable() {
       targetToken = _targetToken;
@@ -19,27 +19,15 @@ contract WeightedTokenDistributor is TokenDistributor {
       }
     }
 
-    function getStakeHolderIndex (address _stakeHolder) public view returns (uint256 _index) {
-      _index = stakeHolders.length+1; //Indicate not found
-      for (uint256 count = 0; count < stakeHolders.length; count++) {
-        if (stakeHolders[count] == _stakeHolder) {
-          _index = count;
-          break;
-        }
-      }
-    }
-
     function getTotalWeight () public view returns (uint256 _total) {
       for (uint256 count = 0; count < stakeHolders.length; count++) {
-        _total = _total.add( stakeHoldersWeight[count] );
+        _total = _total.add( stakeHoldersWeight[stakeHolders[count]] );
       }
     }
 
-    function getPortion (uint256 _total, address _stakeHolder) public view returns (uint256) {
-      uint256 index = getStakeHolderIndex(_stakeHolder);
-      uint256 weight = stakeHoldersWeight[index];
-      uint256 totalWeight = getTotalWeight();
-      return (_total.mul(weight)).div(totalWeight);
+    function getPortion (uint256 _total, uint256 _totalWeight, address _stakeHolder) public view returns (uint256) {
+      uint256 weight = stakeHoldersWeight[_stakeHolder];
+      return (_total.mul(weight)).div(_totalWeight);
     }
 
     function getPortion (uint256 _total) public view returns (uint256) {
@@ -47,7 +35,7 @@ contract WeightedTokenDistributor is TokenDistributor {
     }
 
     function setStakeholder (address _stakeHolder, uint256 _weight) internal onlyOwner returns (bool) {
-      stakeHoldersWeight.push(_weight);
+      stakeHoldersWeight[_stakeHolder] = _weight;
       require(super.setStakeholder(_stakeHolder));
       return true;
     }
@@ -63,13 +51,14 @@ contract WeightedTokenDistributor is TokenDistributor {
 
     function distribute (address _token) public returns (bool) {
         uint256 balance = getTokenBalance(_token);
+        uint256 totalWeight = getTotalWeight();
 
         if (balance < 1) {
           emit InsufficientTokenBalance( _token, block.timestamp );
           return false;
         } else {
           for (uint256 count = 0; count < stakeHolders.length; count++) {
-            uint256 perStakeHolder = getPortion(balance, stakeHolders[count]);
+            uint256 perStakeHolder = getPortion(balance, totalWeight, stakeHolders[count]);
             _transfer(_token, stakeHolders[count], perStakeHolder);
           }
 
